b478a41c7c1a00fa0b44e9e0ddacd7e04573ea59
michiharu <michi1diver@gmail.com>
2025-03-20 04:22:15 +0900

    :white_check_mark: add tests of fromCloudWatchLogsData

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,13 @@
-import { Context } from 'aws-lambda';
+import { CloudWatchLogsDecodedData, Context } from 'aws-lambda';
 import * as cwllink from './index';
 
 const base = `https://region.console.aws.amazon.com/cloudwatch/home?region=region`;
-const groupPart = 'log-groups/log-group/LOG_GROUP';
-const eventPart = 'log-events/LOG_EVENT';
-const termPart = 'filterPattern$3D$2522REQUEST_ID$2522';
+const groupId = 'LOG_GROUP';
+const groupPart = `log-groups/log-group/${groupId}`;
+const eventId = 'LOG_EVENT';
+const eventPart = `log-events/${eventId}`;
+const termId = 'REQUEST_ID';
+const termPart = (id: string = termId) => `filterPattern$3D$2522${id}$2522`;
 const startPart = 'start$3D1649602800000';
 const startRelativePart = 'start$3D-3600000';
 const endPart = 'end$3D1649689199000';
@@ -20,7 +23,7 @@ describe('cwllink.create()', () => {
 
   test(`create('region', 'LOG_GROUP', 'LOG_EVENT', { terms: ['REQUEST_ID'] })`, () => {
     expect(cwllink.create('region', 'LOG_GROUP', 'LOG_EVENT', { terms: ['REQUEST_ID'] })).toBe(
-      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart}`
+      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart()}`
     );
   });
 
@@ -44,12 +47,12 @@ describe('cwllink.create()', () => {
 
   test(`create('region', 'LOG_GROUP', 'LOG_EVENT', { terms, start, end })`, () => {
     const options: cwllink.FilterOptions = {
-      terms: ['REQUEST_ID'],
+      terms: [termId],
       start: 1_649_602_800_000,
       end: 1_649_689_199_000,
     };
     expect(cwllink.create('region', 'LOG_GROUP', 'LOG_EVENT', options)).toBe(
-      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart}$26${startPart}$26${endPart}`
+      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart()}$26${startPart}$26${endPart}`
     );
   });
 });
@@ -60,10 +63,10 @@ describe('cwllink.fromLambdaContext()', () => {
     const context: Pick<Context, 'logGroupName' | 'logStreamName' | 'awsRequestId'> = {
       logGroupName: 'LOG_GROUP',
       logStreamName: 'LOG_EVENT',
-      awsRequestId: 'REQUEST_ID',
+      awsRequestId: termId,
     };
     expect(cwllink.fromLambdaContext(context as Context)).toBe(
-      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart}`
+      `${base}#logsV2:${groupPart}/${eventPart}$3F${termPart()}`
     );
   });
 });
@@ -73,5 +76,37 @@ describe('cwllink.decodeCloudWatchLogsData()', () => {
     // Buffer.from(zlib.gzipSync(`{"message":"こんにちは。"}`)).toString('base64');
     const data = 'H4sIAAAAAAAAE6tWyk0tLk5MT1WyUnrcOPlx0+THjasfNy583Lj+cUOTUi0Allde1iAAAAA=';
     expect(await cwllink.decodeCloudWatchLogsData(data)).toEqual({ message: 'こんにちは。' });
+  });
+});
+
+describe('cwllink.fromCloudWatchLogsData()', () => {
+  const createDecodedData = (message: string): CloudWatchLogsDecodedData => {
+    return {
+      messageType: 'DATA_MESSAGE',
+      owner: 'owner-id',
+      logGroup: groupId,
+      logStream: eventId,
+      subscriptionFilters: ['abcd1234'],
+      logEvents: [
+        {
+          id: 'abcd1234',
+          timestamp: 0,
+          message: message,
+        },
+      ],
+    };
+  };
+
+  test('init error log', async () => {
+    const decoded = createDecodedData('2025-03-01T-00:00:00.000Z\tundefined\tERROR\n');
+    const link = cwllink.fromCloudWatchLogsData(decoded);
+    expect(link).toBe(`${base}#logsV2:${groupPart}/${eventPart}`);
+  });
+
+  test('request error log', async () => {
+    const uuid4 = '01234567-89ab-cdef-0123-456789abcdef';
+    const decoded = createDecodedData(`2025-03-01T-00:00:00.000Z\t${uuid4}\tERROR\n`);
+    const link = cwllink.fromCloudWatchLogsData(decoded);
+    expect(link).toBe(`${base}#logsV2:${groupPart}/${eventPart}$3F${termPart(uuid4)}`);
   });
 });
